a344f01b2db77cd5b776a4b37fc44b388dae10a9
Minii <steffen.begemann@web.de>
2020-02-14 20:00:55 +0100

    CreateBrain in sprachassistent.py integrieren

--- sprachassistent.py
+++ sprachassistent.py
@@ -28,9 +28,27 @@ if Brain.is_file():                                                 #Wenn die Da
     connection = sqlite3.connect("Brain.sqlite")
     cursor = connection.cursor()
 elif input("Soll eine neue Datenbank angelegt werden(j/n)") == "j": #Wenn eine neue Datenbank angelegt werden soll...
-    exec(open("CreateBrain.py").read())                             #Datenbank mit CreateBrain.py erstellen
     connection = sqlite3.connect("Brain.sqlite")
     cursor = connection.cursor()
+    sql_command = """
+    CREATE TABLE verbenregel (
+    ID INTEGER PRIMARY KEY,
+    befehl VARCHAR(30),
+    grundform VARCHAR(30),
+    type VARCHAR(30));"""
+
+    cursor.execute(sql_command)
+
+    sql_command = """
+    CREATE TABLE andere (
+    ID INTEGER PRIMARY KEY,
+    name VARCHAR(30),
+    type VARCHAR(30),
+    befehl VARCHAR(30));"""
+
+    cursor.execute(sql_command)
+    #save changes
+    connection.commit()
 else:                                                               #Ausgeben wenn keine Verbindung zu einer Datenbank hergestell wurde, Programm verlassen
     print("Es wurde keine Verbindung zu einer Datenbank hergestellt. Das Programm wird verlassen.")
     exit()
